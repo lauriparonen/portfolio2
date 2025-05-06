bc0b937cd59c2324b53df840d7083361605071a2
Lauri Paronen <lauriparonen0@gmail.com>
2025-05-06 15:18:29 +0300

    start working on gallery

--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -1,7 +1,22 @@
 const Gallery = () => (
     <div className="min-h-screen px-6 py-20">
       <h2 className="text-3xl font-serif mb-4">Gallery</h2>
-      <p className="text-gray-400">coming soon...</p>
+      <p className="text-gray-400 max-w-xl mb-8">
+        In 2022, OpenAI published DALL-E 2, a now-deprecated image generation model that could create images from text prompts. 
+        This was a very brief period of time when AI image generation wasn't <i>slop</i> yet—each image seemed pregnant with 
+        a promise of infinite possibility and creative freedom, and this potentiality fragranced each generation with a sense of aesthetic richness
+        which compensated for the abundant visual inconsistencies and imperfections.
+        <br /><br />
+        Brian Eno noted that <a href="https://www.goodreads.com/quotes/649039-whatever-you-now-find-weird-ugly-uncomfortable-and-nasty-about"
+        
+        className="text-gray-400 hover:underline">
+         "whatever you now find weird, ugly, uncomfortable and nasty about a new medium will surely become its signature."</a> There
+         is certainly no shortage of things to find ugly and nasty about AI image generation as a medium, and I am by no means a 
+        proponent of "AI art"; however I chose to include these generations of mine on this site as a little token of <i>nostalgia</i>,
+        to celebrate the transience of this specific model. I shall always have a soft spot in my memory for it.
+        
+
+      </p>
     </div>
   );
   export default Gallery;
